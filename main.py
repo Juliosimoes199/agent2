92a2bd8d5e6a63dc876303d616ca4febe557106d
Julio Simões <Juliocesarsimoes100@gmail.com>
2025-04-01 11:13:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from flask import Flask, jsonify
 import func
-import cv2
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import Options
-import time
 
 app = Flask(__name__)
 
