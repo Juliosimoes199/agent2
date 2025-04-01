68ae91a96e00375af4d9990614e37c84bb3f70c1
Julio Simões <Juliocesarsimoes100@gmail.com>
2025-04-01 14:11:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Olá do Flask! ams'
+    return 'Olá do Flask!'
 
 @app.route('/tecnico_laboratorio', methods=['GET'])
 def tecnico_laboratorio():
