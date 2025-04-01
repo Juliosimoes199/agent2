bd58aae39a1174cfb3d55c9912783afb53050c4e
Julio Simões <Juliocesarsimoes100@gmail.com>
2025-04-01 11:03:09 +0100

    Update func.py

--- func.py
+++ func.py
@@ -1,6 +1,87 @@
 
-def ola():
-    return "Ola mundo"
+def filto_exames_confirmado():
+    # Configura o modo headless no Chrome
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Executa sem interface gráfica
+    chrome_options.add_argument("--no-sandbox")  # Recomendado para ambientes como VMs
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Evita problemas de memória compartilhada
+    chrome_options.add_argument("--disable-gpu")  # Desativa GPU (apenas para precaução em alguns sistemas)
+
+    # Inicializa o driver com as opções configuradas
+    navegador = webdriver.Chrome(options=chrome_options)
+    
+    try:
+        # Abre a página de login
+        navegador.get('https://akin-lis-app-web.vercel.app/')
+
+        # Preenche as abas de login
+        navegador.find_element("id", "email").send_keys("jpedro@gmail.com")
+        navegador.find_element("id", "password").send_keys("jpe2024")
+
+        # Clica no botão de login
+        wait = WebDriverWait(navegador, 30)
+        element = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/section/div/div/div[2]/form/button')))
+        element.click()
+
+        # Espera até que o botão "Agendamentos" esteja presente
+        agendamentos_button = WebDriverWait(navegador, 10000).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[2]/div/div[2]/div/ul/li[1]/button"))
+    )
+    # Clique no botão "Agendamentos" ou faça outra ação necessária
+        print("Apareceu")
+        # Captura uma captura de tela da página
+        navegador.save_screenshot('screenshot.png')
+
+        # Carrega a captura de tela e a imagem do elemento
+        screenshot = cv2.imread('screenshot.png')
+        element_image = cv2.imread('ct.png')
+
+        # Usa OpenCV para localizar a posição do elemento na captura de tela
+        result = cv2.matchTemplate(screenshot, element_image, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+        # Desenha um retângulo ao redor do elemento encontrado
+        top_left = max_loc
+        bottom_right = (top_left[0] + element_image.shape[1], top_left[1] + element_image.shape[0])
+        cv2.rectangle(screenshot, top_left, bottom_right, (0, 255, 0), 2)
+        click_script = f"document.elementFromPoint({bottom_right[0]}, {bottom_right[1]}).click();"
+        navegador.execute_script(click_script)
+        #time.sleep(20)
+        #navegador.find_element(By.XPATH, '//*[@id="radix-:ri:"]/ul/li[3]/a/span').click()
+
+        navegador.save_screenshot('screenshot.png')
+
+        # Carrega a captura de tela e a imagem do elemento
+        screenshot = cv2.imread('screenshot.png')
+        element_image = cv2.imread('confirmados.png')
+
+        # Usa OpenCV para localizar a posição do elemento na captura de tela
+        result = cv2.matchTemplate(screenshot, element_image, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+        # Desenha um retângulo ao redor do elemento encontrado
+        top_left = max_loc
+        bottom_right = (top_left[0] + element_image.shape[1], top_left[1] + element_image.shape[0])
+        cv2.rectangle(screenshot, top_left, bottom_right, (0, 255, 0), 2)
+        click_script = f"document.elementFromPoint({bottom_right[0]}, {bottom_right[1]}).click();"
+        navegador.execute_script(click_script)
+
+        # Salva a imagem resultante
+        cv2.imwrite('resultado.png', screenshot)
+        time.sleep(1)
+        # Retorna a URL atual como resposta
+        current_url = navegador.current_url
+        print("URL atual da aba:", current_url)
+        return jsonify({"status": "sucesso", "url_atual": current_url})
+
+    except TimeoutException:
+        print("O botão 'Agendamentos' não apareceu a tempo.")
+        return jsonify({"status": "erro", "mensagem": "O botão não apareceu a tempo"})
+
+    finally:
+        # Garante que o navegador seja fechado
+        navegador.quit()
+    
 
 
 def ola1():
