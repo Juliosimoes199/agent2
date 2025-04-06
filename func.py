10755cbe23caa831674b876fb2b84a0fde2f2a51
Julio Simões <Juliocesarsimoes100@gmail.com>
2025-04-06 17:20:04 +0100

    Update func.py

--- func.py
+++ func.py
@@ -138,36 +138,6 @@ def ola1():
 
 
 
-# Carrega o modelo de linguagem do spaCy
-nlp = spacy.load("pt_core_news_sm")
-
-def extrair_informacoes_pessoais(texto):
-    doc = nlp(texto)
-    informacoes_pessoais = {
-        "nomes": [],
-        "emails": [],
-        "telefones": [],
-    }
-    outras_entidades = []
-
-    # Extrai entidades nomeadas e organiza por tipos
-    for ent in doc.ents:
-        if ent.label_ == "PER":  # Nome de pessoas
-            informacoes_pessoais["nomes"].append(ent.text)
-        else:
-            outras_entidades.append({"texto": ent.text, "tipo": ent.label_})
-
-    # Busca por e-mails no texto usando regex
-    emails = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", texto)
-    informacoes_pessoais["emails"].extend(emails)
-
-    # Busca por números de telefone no texto usando regex
-    telefones = re.findall(r"\b\d{8,11}\b", texto)
-    informacoes_pessoais["telefones"].extend(telefones)
-
-
-    return informacoes_pessoais, outras_entidades
-
 
 
 
