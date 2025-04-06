02319a4422879fa4f2e68edc2a1d65cb3e48b5ae
Julio Simões <Juliocesarsimoes100@gmail.com>
2025-04-06 18:18:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,95 +6,124 @@ import re
 import func
 
 app = Flask(__name__)
-nlp = spacy.load("pt_core_news_md")  # Carrega o modelo Spacy uma vez
-
-def extrair_informacoes_pessoais(texto):
-    doc = nlp(texto)
-    informacoes_pessoais = {
-        "nomes": [],
-        "emails": [],
-        "telefones": [],
-    }
-    outras_entidades = []
-
-    # Extrai entidades nomeadas e organiza por tipos
-    for ent in doc.ents:
-        if ent.label_ == "PER":  # Nome de pessoas
-            informacoes_pessoais["nomes"].append(ent.text)
-        else:
-            outras_entidades.append({"texto": ent.text, "tipo": ent.label_})
-
-    # Busca por e-mails no texto usando regex
-    emails = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", texto)
-    informacoes_pessoais["emails"].extend(emails)
-
-    # Busca por números de telefone no texto usando regex
-    telefones = re.findall(r"\b\d{8,11}\b", texto)
-    informacoes_pessoais["telefones"].extend(telefones)
-
-    return informacoes_pessoais, outras_entidades
+nlp = spacy.load("pt_core_news_md")  # Carrega o modelo Spacy uma vez
 
 @app.route('/')
 def hello_world():
-    return 'Olá do Flask!'
+    return 'Olá do Flask!'
+
 
 @app.route('/chefe_laboratorio', methods=['POST'])
 def tecnico_laboratorio():
-    texto = request.form['texto']
-    email = request.form['email']
-    infor = texto
-    password = request.form['password']
-    def analisar_texto(texto, palavras_chave):
-        resultados = []
-        doc = nlp(texto)
+    texto = request.form['texto']
+    email = request.form['email']
+    infor = texto
+    password = request.form['password']
 
-        for palavra in palavras_chave:
-            palavra_doc = nlp(palavra)
-            for token in doc:
-                similaridade_semantica = token.similarity(palavra_doc) >= 0.75
-                similaridade_textual = fuzz.ratio(token.text.lower(), palavra.lower()) >= 75
+    def analisar_texto(texto, palavras_chave):
 
-                if similaridade_semantica or similaridade_textual:
-                    resultados.append(palavra)
-        return list(set(resultados)) # Retorna apenas palavras-chave únicas encontradas
+        resultados = []
 
-    palavras_chave = ["filtro", "exames", "confirmados",
-                        "alocação", "técnico", "laboratório", "Aloque",
-                        "perfis", "pacientes",
-                        "editar", "edição", "dados", "informação",
-                        "histórico", "próximos",
-                        "inicializar", "análise", "imagens",
-                        "microscópica", "automatizada", "manual",
-                        "manualmente",
-                        "geração", "gerar", "laudo",
-                        "envio", "remover", "eliminar",
-                        "monitorar", "monitoramento", "actividades",
-                        "Acção", "movimento", "trabalho", "faça"]
+        doc = nlp(texto)
 
-    #texto = "Analise manual mais exames e exames laboratorias tambem"
-    resultados = analisar_texto(texto, palavras_chave)
-    if ("filtro" in resultados) & ("exames" in resultados):
-        url = func.filto_exames_confirmado(email, password)
-        return jsonify({"status": resultados, "url": url})
 
-    elif ("filtro" in resultados) & (("perfis" in resultados) or ("pacientes" in resultados)):
-        informacoes, outras_entidades = extrair_informacoes_pessoais("júlio")
-        nomes = informacoes['nomes']
-        nomes= nomes[0]
 
-        #nome = "Kuenda"
-        url = func.filtro_pacientes(nomes, password, email)
-        return jsonify({"status": resultados, "url":url, "nomes":nomes})
-    else:
-        return "Não tem"
+        for palavra in palavras_chave:
+            palavra_doc = nlp(palavra)
+            for token in doc:
+                similaridade_semantica = token.similarity(palavra_doc) >= 0.75
+                similaridade_textual = fuzz.ratio(token.text.lower(), palavra.lower()) >= 75
 
-    #return jsonify({"status": resultados})
+                if similaridade_semantica or similaridade_textual:
+                    resultados.append(palavra)
+
+        return list(set(resultados)) # Retorna apenas palavras-chave únicas encontradas
+
+
+    palavras_chave = ["filtro", "exames", "confirmados",
+                      "alocação", "técnico", "laboratório", "Aloque",
+                      "perfis", "pacientes",
+                      "editar", "edição", "dados", "informação",
+                      "histórico", "próximos",
+                      "inicializar", "análise", "imagens",
+                      "microscópica", "automatizada", "manual",
+                      "manualmente",
+                      "geração", "gerar", "laudo",
+                      "envio", "remover", "eliminar",
+                      "monitorar", "monitoramento", "actividades",
+                      "Acção", "movimento", "trabalho", "faça"]
+
+
+
+    #texto = "Analise manual mais exames e exames laboratorias tambem"
+    resultados = analisar_texto(texto, palavras_chave)
+
+    if ("filtro" in resultados) & ("exames" in resultados):
+        url = func.filto_exames_confirmado(email, password)
+        return jsonify({"status": resultados, "url": url})
+
+
+
+    elif ("filtro" in resultados) & (("perfis" in resultados) or ("pacientes" in resultados)):
+        def extrair_informacoes_pessoais(texto):
+            doc = nlp(texto)
+            informacoes_pessoais = {
+            "nomes": [],
+            "emails": [],
+            "telefones": [],
+            }
+
+            outras_entidades = []
+
+    # Extrai entidades nomeadas e organiza por tipos
+
+            for ent in doc.ents:
+                if ent.label_ == "PER":  # Nome de pessoas
+                    informacoes_pessoais["nomes"].append(ent.text)
+                else:
+                    outras_entidades.append({"texto": ent.text, "tipo": ent.label_})
+
+    # Busca por e-mails no texto usando regex
+
+            emails = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", texto)
+            informacoes_pessoais["emails"].extend(emails)
+
+    # Busca por números de telefone no texto usando regex
+
+            telefones = re.findall(r"\b\d{8,11}\b", texto)
+            informacoes_pessoais["telefones"].extend(telefones)
+
+            return informacoes_pessoais, outras_entidades
+
+        global infor
+        informacoes, outras_entidades = extrair_informacoes_pessoais(infor)
+        nomes = informacoes['nomes']
+        nomes= nomes[0] 
+
+        #nome = "Kuenda"
+
+        url = func.filtro_pacientes(nomes, password, email)
+        return jsonify({"status": resultados, "url":url, "nomes":nomes})
+
+    else:
+
+        return "Não tem"
+
+        
+
+    #return jsonify({"status": resultados})
+
 
 @app.route('/ver', methods=['GET'])
 def ver_rota():
-    # s = func.ola1()  # Remova se não estiver usando func
-    return "Rota /ver" # Adicione um retorno padrão
+    # s = func.ola1()  # Remova se não estiver usando func
+
+    return "Rota /ver" # Adicione um retorno padrão
+
+
 
 # Executa o servidor Flask
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Permite acesso externo
+
+    app.run(host='0.0.0.0', port=5000)  # Permite acesso externo
