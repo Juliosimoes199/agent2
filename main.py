812db06392c4eb217518894ad868c378afdca959
Julio Simões <Juliocesarsimoes100@gmail.com>
2025-04-06 17:38:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def tecnico_laboratorio():
             return informacoes_pessoais, outras_entidades
 
 
-        informacoes, outras_entidades = extrair_informacoes_pessoais(texto)
+        informacoes, outras_entidades = extrair_informacoes_pessoais("Júlio cesar em muito fixe")
         nomes = informacoes['nomes']
         nomes= nomes[0]
         
